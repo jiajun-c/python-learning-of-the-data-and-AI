5d8d38c2fc73874478ce792c30f52735e2262371
jiajun-c <2966623227@qq.com>
2022-01-10 00:51:47 +0800

    For the part of the list operation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # python-learning-of-the-data-and-AI
 My learning of the AI and data in 2022 winter start from the data.
 ## the basic data operation1
+All the markdown file will have the relevant the .py file that is executable
+
 [string operation](./basic/string_operation.md)
 
 [time](./basic/time.md)
+
+[list](./basic/List.md)
--- basic/List.py
+++ basic/List.py
@@ -8,3 +8,31 @@ print(a[:4])
 
 b = a
 print(b)
+
+# the in and the not function in the list
+if 1 in a:
+    print("1 is in the a")
+
+# the use of add or delete the elem in the list
+a.append(7)
+print(a)
+a.remove(7)
+print(a)
+a.pop()
+print(a)
+
+
+def cmp(b):
+    return b
+
+
+k = [3, 4, 1, 3]
+k = sorted(k)
+# the default is decrease
+print(k)
+
+k = sorted(k, reverse=True)
+print(k)
+
+sorted(a, key=cmp)
+print(a)
